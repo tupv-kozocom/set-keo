8f395925b58bbb0445470d9ac8918a83dd59b5f9
TuPVKozocom <tupv@kozo-japan.com>
2025-03-26 14:17:27 +0700

    Add shadcn ui

--- src/components/Form/Button.tsx
+++ src/components/Form/Button.tsx
@@ -1,18 +1,15 @@
 type ButtonProps = {
-    type: "submit" | "reset" | "button";
-    className?: string;
-    name: string;
-}
+  type: 'submit' | 'reset' | 'button';
+  className?: string;
+  name: string;
+};
 
 function Button({ type, className, name }: ButtonProps) {
-    return (
-        <button
-            type={type}
-            className={`text-white bg-orange-500 w-full my-6 p-3 rounded-lg ${className}`}
-        >
-            {name}
-        </button>
-    );
+  return (
+    <button type={type} className={`text-white bg-orange-500 w-full my-6 p-3 rounded-lg ${className}`}>
+      {name}
+    </button>
+  );
 }
 
 export default Button;
