0f1ddfaa273cface9b35e0880d05208f66d1664a
quangtd-kozocom <quangtd@kozo-japan.com>
2025-03-27 14:18:30 +0700

    SK-Q02: Add PostOwner page

--- src/app/posts/[id]/owner/page.tsx
+++ src/app/posts/[id]/owner/page.tsx
@@ -2,11 +2,10 @@ import PostCarousel from "@/components/Post/PostCarousel";
 import PostBreadcrumb from "@/components/Post/PostBreadcrumb";
 import PostDetailContentHeader from "@/components/Post/PostDetail/PostDetailContentHeader";
 import PostDetailMarkdown from "@/components/Post/PostDetail/PostDetailMarkdown";
-import InvitationList from "@/components/Post/PostInvited/InvitationList";
-import {Button} from "@/components/ui/button";
 import PostLocation from "@/components/Post/PostLocation";
+import InvitationList from "@/components/Post/PostInvited/InvitationList";
 
-function PostInvited() {
+function PostOwner() {
     return (
         <div className="container max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between gap-8 mt-4">
             {/*Left content*/}
@@ -23,14 +22,10 @@ function PostInvited() {
             <div className="hidden lg:w-[30%] lg:block">
                 <aside className="bg-white rounded-lg p-4">
                     <InvitationList/>
-                    <div className="flex gap-4 justify-between mt-4">
-                        <Button className="flex-grow text-white">Tham gia</Button>
-                        <Button className="flex-grow bg-gray-400 text-white">Từ chối</Button>
-                    </div>
                 </aside>
             </div>
         </div>
     );
 }
 
-export default PostInvited;
+export default PostOwner;
